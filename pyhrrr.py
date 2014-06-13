7bef3008be7ba3e07d14a4f44530e719f3d3ad3a
mattjohnson <mjohnson541@gmail.com>
2014-06-13 14:33:07 -0500

    added global variables for hrrr pressures and variables

--- pyhrrr.py
+++ pyhrrr.py
@@ -11,6 +11,19 @@ HRRR directory.
 """
 
 import os
+import numpy as np
+
+global HRRR_PS
+HRRR_PS = np.array([1013, 1000,  975,  950,  925,  900,  875,  850,  825,  800,  775,
+        750,  725,  700,  675,  650,  625,  600,  575,  550,  525,  500,
+        475,  450,  425,  400,  375,  350,  325,  300,  275,  250,  225,
+        200,  175,  150,  125,  100,   75,   50])
+        
+global HRRR_VARS
+HRRR_VARS =['Geopotential Height','Temperature','Relative humidity','Dew point temperature',
+        'Specific humidity','Vertical velocity','U component of wind','V component of wind',
+        'Absolute vorticity','Cloud mixing ratio','Cloud Ice','Rain mixing ratio','Snow mixing ratio',
+        'Graupel (snow pellets)']
 
 wkdir = os.getcwd()
 
