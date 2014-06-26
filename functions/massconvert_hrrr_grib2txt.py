4b71ac19eea749ac3272044727b133e4fffe7ec1
mattjohnson <mjohnson541@gmail.com>
2014-06-26 08:49:52 -0500

    fixed datestring issues

--- functions/massconvert_hrrr_grib2txt.py
+++ functions/massconvert_hrrr_grib2txt.py
@@ -4,7 +4,8 @@ Created on Wed Jun 25 15:54:28 2014
 
 @author: mattjohnson
 """
-
+import numpy as np
+import datetime
 
 def massconvert_hrrr_grib2txt(startdate = None, enddate = None, modelstartindex = 0,directory = None, enddirectory = None, loc = [36.605, -97.485], indexes = None,modelhours = False):
     """
@@ -24,10 +25,15 @@ def massconvert_hrrr_grib2txt(startdate = None, enddate = None, modelstartindex 
     else:
         index2 = datestrings.index(enddate) 
         
-   
+        
         filelists = filelists[index1:index2]
-        datestrings = filelists[index1:index2]
-    
+        datestrings = datestrings[index1:index2]
+        datestrings = [i.date() for i in datestrings]
+        datestrings = np.unique(np.array(datestrings))
+        datestrings = datestrings.tolist()
+        datestrings = [datetime.datetime(i.year,i.month,i.day) for i in datestrings]
+        
+        
     if not modelhours:
         for i in datestrings:
             for j in range(len(filelists[0])):
