c29661848dffc0355e6ae29bd563a1509f60fb5d
Your Name <you@example.com>
2014-06-27 14:02:28 -0500

    Revert "stuff"
    This reverts commit ded206bd8bc3107accfc2e74ab0c2e621ee11b3f.

---
+++ processing/process1.py
@@ -0,0 +1,7 @@
+import datetime
+import pyhrrr
+enddirectory = '/home/mjohnson/python/hrrr_txt'
+directory = '/data/san_store/HRRR'
+startdate = datetime.datetime(2014,4,10)
+enddate = datetime.datetime(2014,5,6)
+pyhrrr.massconvert_hrrr_grib2txt(startdate=startdate,enddate=enddate,hours = [0,4],directory=directory,enddirectory=enddirectory)
