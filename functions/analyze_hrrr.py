4c99c018a23eaa2151030a6d2c017fb2be9f2557
mattjohnson <mjohnson541@gmail.com>
2014-06-30 13:34:12 -0500

    data analysis and variable storage developments

--- functions/analyze_hrrr.py
+++ functions/analyze_hrrr.py
@@ -5,10 +5,13 @@ Created on Mon Jun 30 09:14:32 2014
 @author: mattjohnson
 """
 import os
-import datetime
 
-def analyze_hrrr(analysis_day = data,analysis_hour = analysis,analysis_overall = analysish,hourlists,directory=os.getcwd(),loc,indexes):
+
+def analyze_hrrr(dates,analysis_day = 'data',analysis_hour = 'analysis',analysis_overall = 'analysish',hourlists = [1],directory=os.getcwd(),loc = [36.605,-97.485],indexes = None):
     """
+    flexible analysis function for analyzing hrrr text files on several different levels, analysis strings are lines of executable code
+    that are run and the returned value becomes the analysis for that level, if a level is ignored it will compile all of the data into a list for that level
+    allowing analysis to take place at the next level
     """
     
     analysisd = []
