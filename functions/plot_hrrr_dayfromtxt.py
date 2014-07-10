e243a97d1c4c1f88279d6f1bb2c5960dc5ccbd89
mattjohnson <mjohnson541@gmail.com>
2014-07-10 10:01:26 -0500

    times fixes

--- functions/plot_hrrr_dayfromtxt.py
+++ functions/plot_hrrr_dayfromtxt.py
@@ -12,11 +12,16 @@ import matplotlib.pyplot as plt
 
 
 
-def plot_hrrr_dayfromtxt(matrix,final_unit,date = None,loc = None,ps = HRRR_PS, hour=0, scaling = 1,figsize = [15,8]):
+def plot_hrrr_dayfromtxt(matrix,final_unit,timedata,date = None,loc = None,ps = HRRR_PS, hour=0, scaling = 1,figsize = [15,8]):
     
-    
-    times = np.array(range(matrix.shape[0]+1))
-    
+    if timedata != None:
+        datetimes = matplotlib.dates.num2date(timedata)
+        timeshift = datetime.timedelta(hours=hour)
+        hrrr_hours = [(c+timeshift).hour for c in datetimes]
+        times = hrrr_hours
+    else:
+        times = np.array(range(matrix.shape[0]+1))
+        
     from matplotlib.ticker import MultipleLocator, FormatStrFormatter
         
     values = np.array(matrix)
