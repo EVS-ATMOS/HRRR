404df240a2f664de91428f1c7c90da5780cc3340
mattjohnson <mjohnson541@gmail.com>
2014-07-15 09:48:31 -0500

    quick fix

--- functions/compress_radartohrrr.py
+++ functions/compress_radartohrrr.py
@@ -76,7 +76,7 @@ def compress_radartohrrr(radar_filename, sounding_filename, radar_directory=os.g
         date = datetime.datetime(int(radar_filename[15:19]),int(radar_filename[19:21]),int(radar_filename[21:23]))
         filestring = produce_radar_txt_string(date)
         g = open(filestring,'w')
-        u = [z.tolist(),hrrr_heights.tolist(),tsinds,hsinds]
+        u = [z,hrrr_heights.tolist(),tsinds,hsinds]
         json.dump(u,g)
         g.close()
         os.chdir(wkdir)
