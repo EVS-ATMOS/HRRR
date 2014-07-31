410dfac047cf0069f37dcd7d0296fdac5f18de7e
mattjohnson <mjohnson541@gmail.com>
2014-07-31 11:03:48 -0500

    fixes

--- functions/write_hrrr_hrrr2txt.py
+++ functions/write_hrrr_hrrr2txt.py
@@ -89,16 +89,17 @@ def write_hrrr_grib2txt(date=datetime.datetime.now(),filenum = 24,hour = 0,direc
                     try:
                         data.pop(i)
                         dates.pop(i)
-                        i = i+2
-                    except IndexError:
                         i = i+1
+                    except IndexError:
+                        pass
         else:
             try:
                 data.pop(i)
                 dates.pop(i)
-                i = i+2
-            except IndexError:
                 i = i+1
+            except IndexError:
+                pass
+        i = i+1
             
     try:
         json.dump([data,dates,parameterlist,loc,indexes,units],f)
