a4bc213be5c0eb404489903e8744a2233c516d96
mattjohnson <mjohnson541@gmail.com>
2014-07-31 16:08:34 -0500

    tweaks

--- processing/process1.py
+++ processing/process1.py
@@ -2,6 +2,6 @@ import datetime
 import pyhrrr
 enddirectory = '/home/mjohnson/python/hrrr_txt'
 directory = '/data/san_store/HRRR'
-startdate = datetime.datetime(2014,5,28)
-enddate = datetime.datetime(2014,6,18)
+startdate = datetime.datetime(2014,7,24)
+enddate = datetime.datetime(2014,7,25)
 pyhrrr.massconvert_hrrr_grib2txt(startdate=startdate,enddate=enddate,hours = [1],directory=directory,enddirectory=enddirectory)
--- processing/txtfileprocessing.py
+++ processing/txtfileprocessing.py
@@ -23,7 +23,7 @@ import datetime
 
 n = 4
 
-startdate = datetime.datetime(2014,5,7)
+startdate = datetime.datetime(2014,7,23)
 
 enddate = datetime.datetime(2014,7,30)
 
